8f0600da52be10f502f2d7b5dca6e4481920a36e
Arun Gabriel Baby <arun.gaberial.baby@accenture.com>
2017-04-06 20:35:42 -0400

    changed to chrome webdriver

--- AccountLogin/src/test/java/com/acn/account/steps/context/StepContext.java
+++ AccountLogin/src/test/java/com/acn/account/steps/context/StepContext.java
@@ -1,24 +1,32 @@
 package com.acn.account.steps.context;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 
 public class StepContext {
 
-	private static String APP_PORT = System.getProperty("app.accountlogin.port");
-	private static String APP_URI = "http://localhost:"+ APP_PORT + "/";	
+	private static String APP_URI = System.getProperty("app.accountlogin.uri") + "/";
+	private static String CHROME_DRIVER_URI = System.getProperty("app.chromedriver.uri");
 	private static boolean initialized = false;
 	private WebDriver driver = null;
 
 	@Before
-	public void setUp() {
-		
-	/*	System.setProperty("webdriver.chrome.driver",
-				"C:/Users/White Tiger/Documents/Arun/chromedriver_win32/chromedriver.exe");*/
-		driver = new ChromeDriver();
+	public void setUp() throws MalformedURLException {
+
+		/*
+		 * -Dtest=CucumberTest
+		 * -Dapp.accountlogin.uri=http://localhost:8080
+		 * -Dapp.chromedriver.uri=http://localhost:9515 test
+		 */
+		driver = new RemoteWebDriver(new URL(CHROME_DRIVER_URI), DesiredCapabilities.chrome());		
 	}
 
 	public WebDriver getWebDriver() {
